5c163eff47c268918dcc6f0095272c4e6a935406
Simke <44616974+cubalevski@users.noreply.github.com>
2020-10-24 18:56:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function validateForm() {
+    var x = document.forms["form1"]["mileage"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
+
+function myFunction() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("text");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+  
+  function calculate()
+{
+        num1 = document.getElementById("mileage").value;
+        num2 = document.getElementById("AverageFuel").value;
+        num3 = document.getElementById("FuelPrice").value;
+        num4 = document.getElementById("Additionalcost").value;
+        num5 = document.getElementById("Passengers").value;
+        document.getElementById("result").innerHTML = ((((num1 / 100) * num2) * num3 + num4 ) / num5);
+        localStorage.setItem("result")
+}
+
